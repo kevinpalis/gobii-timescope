2438d14e2d27b666fc8207778dc42e0fe3a45b35
fvgoldman <fav26@cornell.edu>
2019-06-14 09:48:16 -0400

    Implemented functionality to restart web application of TomCat in response to details provided in web.xml

--- src/org/gobiiproject/datatimescope/webconfigurator/xmlModifier.java
+++ src/org/gobiiproject/datatimescope/webconfigurator/xmlModifier.java
@@ -1,7 +1,10 @@
 package org.gobiiproject.datatimescope.webconfigurator;
 
+import org.apache.catalina.ant.ReloadTask;
 import org.w3c.dom.*;
 import org.xml.sax.SAXException;
+import java.lang.Object;
+import org.apache.tools.ant.Task;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -17,22 +20,45 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+
 public class xmlModifier {
+
+    private static String ldapUserForUnitTestXPath = "//ldapUserForUnitTest";
+    private static String ldapPasswordForUnitTestXPath = "//ldapPasswordForUnitTest";
+    private static String gobiiAuthenticationTypeXPath = "//gobiiAuthenticationType";
+    private static String ldapUserDnPatternXPath = "//ldapUserDnPattern";
+    private static String ldapUrlXPath = "//ldapUrl";
+    private static String ldapBindUserXPath = "//ldapBindUser";
+    private static String ldapBindPasswordXPath = "//ldapBindPassword";
+    private static String ldapUserForBackendProcsXPath = "//ldapUserForBackendProcs";
+    private static String ldapPasswordForBackendProcsXPath = "//ldapPasswordForBackendProcs";
+    private static String emailSvrTypeXPath = "//emailSvrType";
+    private static String emailSvrDomainXPath = "//emailSvrDomain";
+    private static String emailSvrUserXPath = "//emailSvrUser";
+    private static String emailSvrHashTypeXPath = "//emailSvrHashType";
+    private static String emailSvrPasswordXPath = "//emailSvrPassword";
+    private static String emailServerPortXPath = "//emailServerPort";
+
+    //Finds the GOBII_WEB configs
+    private static String hostForReloadXPath = "//serverConfig/serverType[text() = 'GOBII_WEB']/following-sibling::host";
+    private static String contextPathlForReloadXPath = "//serverConfig/serverType[text() = 'GOBII_WEB']/following-sibling::contextPath]";
+    private static String portForReloadXPath = "//serverConfig/serverType[text() = 'GOBII_WEB']/following-sibling::port";
+
+    private String pathForReload;
 
     /**
      * @param identifier For the use cases in which tag will not return a single node, currently not in use
-     * @param tags ArrayList of tags that will be modified
+     * @param XPathQueries ArrayList of tags that will be modified
      * @param newContents Array of the new Content, to be filled in by the User/UI
      * @return 0 upon at least one successful modification, -1 upon failure to make any changes
      * This function performs the modifications to the xml file according to the provided parameters
      * It will print messages, if anything didn't fully execute specifying what went wrong
      */
-    private static int modifier(String identifier, ArrayList<String> tags, String[] newContents){
-        //path?
-        //Do we want server side data validation? Or will this be covered in the UI, probably better
-        String path = "/home/fvgoldman/gobiidatatimescope/src/org/gobiiproject/datatimescope/webconfigurator/gobii-web.xml";
-        //String path = "/home/fvgoldman/data/gobii_bundle/config/gobii-web.xml";
-        if (tags.size() != newContents.length){
+    private static int modifier(String identifier, ArrayList<String> XPathQueries, String[] newContents) {
+        //String path = "/home/fvgoldman/gobiidatatimescope/src/org/gobiiproject/datatimescope/webconfigurator/gobii-web.xml";
+        String path = "/data/gobii_bundle/config/gobii-web.xml";
+
+        if (XPathQueries.size() != newContents.length) {
             //Something didn't line up and will definitely break the XML
             System.out.println("The amount of tags does not line up with the amount of contents.");
             return -1;
@@ -41,13 +67,12 @@ public class xmlModifier {
         //Build DOM
         Document doc = xmlModifier.retrieveFile(path);
 
-        for (int i = 0; i < tags.size(); i++) {
-            String tag = tags.get(i);
+        for (int i = 0; i < XPathQueries.size(); i++) {
             String newContent = newContents[i];
-            String expression = "//" + tag;
-            NodeList nodes = evaluateExpression(expression, doc);
-            if (nodes == null||nodes.getLength() == 0){
-                System.out.println("There seems to be no result for the tag: " + tag);
+
+            NodeList nodes = evaluateExpression(XPathQueries.get(i), doc);
+            if (nodes == null || nodes.getLength() == 0) {
+                System.out.println("There seems to be no result for the expression: " + XPathQueries.get(i));
 
             } else if (nodes.getLength() > 1) {
                 //We need to use another form of verification
@@ -65,6 +90,20 @@ public class xmlModifier {
         }
 
         xmlModifier.modifyDocument(doc, path);
+        try {
+            String host = evaluateExpression(hostForReloadXPath, doc).item(0).getTextContent();
+            String contextPath = evaluateExpression(contextPathlForReloadXPath, doc).item(0).getTextContent();
+            String port = evaluateExpression(portForReloadXPath, doc).item(0).getTextContent();
+            ReloadTask command = new ReloadTask();
+            command.setUsername("gadm");
+            command.setPassword("g0b11admin");
+            command.setUrl(host + contextPath + port);
+            //Ask about this
+            command.setPath("/gobiidatatimescope/WebContent/WEB-INF/web.xml");
+            command.execute();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
         return 0;
     }
 
@@ -116,17 +155,16 @@ public class xmlModifier {
         ArrayList<String> tagsToBeModified;
         switch (operation){
             case "LDAP Unit":
-                //content will be filled by UI upon calling
-                tagsToBeModified = new ArrayList<>(Arrays.asList("ldapUserForUnitTest", "ldapPasswordForUnitTest"));
+                tagsToBeModified = new ArrayList<>(Arrays.asList(ldapUserForUnitTestXPath, ldapPasswordForUnitTestXPath));
                 break;
             case "LDAP Gobii":
                 tagsToBeModified = new ArrayList<>(Arrays.asList(
-                        "gobiiAuthenticationType", "ldapUserDnPattern", "ldapUrl",
-                        "ldapBindUser", "ldapBindPassword", "ldapUserForBackendProcs", "ldapPasswordForBackendProcs"));
+                        gobiiAuthenticationTypeXPath, ldapUserDnPatternXPath, ldapUrlXPath, ldapBindUserXPath,
+                        ldapBindPasswordXPath, ldapUserForBackendProcsXPath, ldapPasswordForBackendProcsXPath));
                 break;
             case "Email notification":
-                tagsToBeModified = new ArrayList<>(Arrays.asList("emailSvrType", "emailSvrDomain", "emailSvrUser",
-                        "emailSvrHashType", "emailSvrPassword", "emailServerPort"));
+                tagsToBeModified = new ArrayList<>(Arrays.asList(emailSvrTypeXPath, emailSvrDomainXPath, emailSvrUserXPath,
+                        emailSvrHashTypeXPath, emailSvrPasswordXPath, emailServerPortXPath));
                 break;
             default:
                 //Not implemented yet
@@ -138,38 +176,41 @@ public class xmlModifier {
             System.out.println("Something went wrong.");
         } else {
             System.out.println("All went well.");
-            /*String command = "/home/fvgoldman/Documents/apache-tomcat-7.0.94/bin/startup.sh";
-            try {
-                Process child = Runtime.getRuntime().exec(command);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }*/
         }
 
     }
 
     public static void main(String[] args){
+        //content will be filled by UI upon calling
+
         System.out.println("Test 1");
         String[] testContent_1 = {"user2", "dummypass"};
         xmlModifierWrapper("LDAP Unit",  testContent_1);
+
         System.out.println("Test 2");
         String[] testContent_2 = {"user2"};
         xmlModifierWrapper("LDAP Unit",  testContent_2);
+
         System.out.println("Test 3");
         String[] testContent_3 = {};
         xmlModifierWrapper("Dummy",  testContent_3);
+
         System.out.println("Test 4");
         String[] testContent_4 = {"NO_AUTH", "uid={1}", "localhost:8080", "test", "test", "admin", "123456"};
         xmlModifierWrapper("LDAP Gobii",  testContent_4);
+
         System.out.println("Test 5");
         String[] testContent_5 = {"SMTP", "smtp.gmail.com", "my.test@gmail.com", "MD5", "test", "666"};
         xmlModifierWrapper("Email notification",  testContent_5);
+
         System.out.println("Test 6");
         String[] testContent_6 = {"SMTP", "smtp.gmail.com", "my.test@gmail.com", "MD5", "test", "666"};
         xmlModifierWrapper("LDAP Unit",  testContent_6);
+
         System.out.println("Test 7");
         String[] testContent_7 = {"", ""};
         xmlModifierWrapper("LDAP Unit",  testContent_7);
+
         System.out.println("Test 8");
         String[] testContent_8 = {"user2", "dummypass"};
         xmlModifierWrapper("LDAP Unit",  testContent_8);
