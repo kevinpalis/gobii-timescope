3391358e0f888e465d7ad2ce33819ea1973024c3
fvgoldman <fav26@cornell.edu>
2019-06-17 15:04:36 -0400

    Framework for config is set up, to be completed within the next day

--- src/org/gobiiproject/datatimescope/webconfigurator/xmlModifier.java
+++ src/org/gobiiproject/datatimescope/webconfigurator/xmlModifier.java
@@ -1,10 +1,17 @@
 package org.gobiiproject.datatimescope.webconfigurator;
 
-import org.apache.catalina.ant.ReloadTask;
 import org.w3c.dom.*;
 import org.xml.sax.SAXException;
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.Init;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.Selectors;
+import org.zkoss.zk.ui.util.GenericForwardComposer;
+import org.zkoss.zul.Include;
+
 import java.lang.Object;
-import org.apache.tools.ant.Task;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -19,9 +26,10 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 
-public class xmlModifier {
+public class xmlModifier extends SelectorComposer<Component> {
 
     private static String ldapUserForUnitTestXPath = "//ldapUserForUnitTest";
     private static String ldapPasswordForUnitTestXPath = "//ldapPasswordForUnitTest";
@@ -39,12 +47,157 @@ public class xmlModifier {
     private static String emailSvrPasswordXPath = "//emailSvrPassword";
     private static String emailServerPortXPath = "//emailServerPort";
 
+    private static String path = "/data/gobii_bundle/config/gobii-web.xml";
+
+    private String includeSrc = "/home.zul";
+
+
     //Finds the GOBII_WEB configs
     private static String hostForReloadXPath = "//serverConfig/serverType[text() = 'GOBII_WEB']/following-sibling::host";
     private static String contextPathlForReloadXPath = "//serverConfig/serverType[text() = 'GOBII_WEB']/following-sibling::contextPath]";
     private static String portForReloadXPath = "//serverConfig/serverType[text() = 'GOBII_WEB']/following-sibling::port";
 
     private String pathForReload;
+
+    public void doAfterCompose(Component comp) throws Exception {
+        super.doAfterCompose(comp);
+    }
+
+    @Command("ldapSystemUser")
+    public void ldapSystemUser() {
+        Include include = (Include)Selectors.iterable(getPage(), "#mainContent")
+                .iterator().next();
+        include.setSrc("/ldapSystemUser.zul");
+        getPage().getDesktop().setBookmark("p_"+"ldapUSerSystem");
+    }
+
+    @Command("ldapTestUser")
+    public void ldapTestUser() {
+        Include include = (Include)Selectors.iterable(getPage(), "#mainContent")
+                .iterator().next();
+        include.setSrc("/ldapTestUser.zul");
+        getPage().getDesktop().setBookmark("p_"+"ldapTestUser");
+    }
+
+    @Command("emailNotifications")
+    public void emailNotifications() {
+        Include include = (Include)Selectors.iterable(getPage(), "#mainContent")
+                .iterator().next();
+        include.setSrc("/emailNotifications.zul");
+        getPage().getDesktop().setBookmark("p_"+"emailNotifications");
+    }
+
+    @Command("pushNotifications")
+    public void pushNotifications() {
+        Include include = (Include)Selectors.iterable(getPage(), "#mainContent")
+                .iterator().next();
+        include.setSrc("/pushNotifications.zul");
+        getPage().getDesktop().setBookmark("p_"+"pushNotifications");
+    }
+
+    @Command("pushNotifications")
+    public void pushNotifications() {
+        Include include = (Include)Selectors.iterable(getPage(), "#mainContent")
+                .iterator().next();
+        include.setSrc("/pushNotifications.zul");
+        getPage().getDesktop().setBookmark("p_"+"pushNotifications");
+    }
+
+    public String getldapUserForUnitTest(){
+        Document doc = xmlModifier.retrieveFile(path);
+        return evaluateExpression("//ldapUserForUnitTest", doc).item(0).getTextContent();
+    }
+    public String getldapPasswordForUnitTest(){
+        Document doc = xmlModifier.retrieveFile(path);
+        return evaluateExpression("//ldapPasswordForUnitTest", doc).item(0).getTextContent();
+    }
+    public String getgobiiAuthenticationType(){
+        Document doc = xmlModifier.retrieveFile(path);
+        return evaluateExpression("//gobiiAuthenticationType", doc).item(0).getTextContent();
+    }
+    public String getldapUserDnPattern(){
+        Document doc = xmlModifier.retrieveFile(path);
+        return evaluateExpression("//ldapUserDnPattern", doc).item(0).getTextContent();
+    }
+    public String getldapUrl(){
+        Document doc = xmlModifier.retrieveFile(path);
+        return evaluateExpression("//ldapUrl", doc).item(0).getTextContent();
+    }
+    public String getldapBindUser(){
+        Document doc = xmlModifier.retrieveFile(path);
+        return evaluateExpression("//ldapBindUser", doc).item(0).getTextContent();
+    }
+    public String getldapBindPassword(){
+        Document doc = xmlModifier.retrieveFile(path);
+        return evaluateExpression("//ldapBindPassword", doc).item(0).getTextContent();
+    }
+    public String getldapUserForBackendProcs(){
+        Document doc = xmlModifier.retrieveFile(path);
+        return evaluateExpression("//ldapUserForBackendProcs", doc).item(0).getTextContent();
+    }
+    public String getldapPasswordForBackendProcs(){
+        Document doc = xmlModifier.retrieveFile(path);
+        return evaluateExpression("//ldapPasswordForBackendProcs", doc).item(0).getTextContent();
+    }
+    public String getemailSvrType(){
+        Document doc = xmlModifier.retrieveFile(path);
+        return evaluateExpression("//emailSvrType", doc).item(0).getTextContent();
+    }
+    public String getemailSvrDomain(){
+        Document doc = xmlModifier.retrieveFile(path);
+        return evaluateExpression("//emailSvrDomain", doc).item(0).getTextContent();
+    }
+    public String getemailSvrUser(){
+        Document doc = xmlModifier.retrieveFile(path);
+        return evaluateExpression("//emailSvrUser", doc).item(0).getTextContent();
+    }
+    public String getemailSvrHashType(){
+        Document doc = xmlModifier.retrieveFile(path);
+        return evaluateExpression("//emailSvrHashType", doc).item(0).getTextContent();
+    }
+    public String getemailSvrPassword(){
+        Document doc = xmlModifier.retrieveFile(path);
+        return evaluateExpression("//emailSvrPassword", doc).item(0).getTextContent();
+    }
+    public String getemailServerPort(){
+        Document doc = xmlModifier.retrieveFile(path);
+        return evaluateExpression("//emailServerPort", doc).item(0).getTextContent();
+    }
+
+
+
+    private static List<String> retriever (String identifier, ArrayList<String> XPathQueries) {
+        //String path = "/home/fvgoldman/gobiidatatimescope/src/org/gobiiproject/datatimescope/webconfigurator/gobii-web.xml";
+        //String path = "/data/gobii_bundle/config/gobii-web.xml";
+
+
+        //Build DOM
+        Document doc = xmlModifier.retrieveFile(path);
+        List<String> results = new ArrayList<>();
+
+        for (String xPathQuery : XPathQueries) {
+
+            NodeList nodes = evaluateExpression(xPathQuery, doc);
+            if (nodes == null || nodes.getLength() == 0) {
+                System.out.println("There seems to be no result for the expression: " + xPathQuery);
+
+            } else if (nodes.getLength() > 1) {
+                //We need to use another form of verification
+                if (identifier.equals("")) {
+                    System.out.println("There are more than one entries that fit the given parameters, please specify more precisely which one to modify.");
+                }
+                for (int j = 0; j < nodes.getLength(); j++) {
+                    if (nodes.item(j).getTextContent().equals(identifier)) {
+
+                    }
+                }
+            } else {
+                results.add(nodes.item(0).getTextContent());
+            }
+        }
+
+        return results;
+    }
 
     /**
      * @param identifier For the use cases in which tag will not return a single node, currently not in use
@@ -90,7 +243,7 @@ public class xmlModifier {
         }
 
         xmlModifier.modifyDocument(doc, path);
-        try {
+        /*try {
             String host = evaluateExpression(hostForReloadXPath, doc).item(0).getTextContent();
             String contextPath = evaluateExpression(contextPathlForReloadXPath, doc).item(0).getTextContent();
             String port = evaluateExpression(portForReloadXPath, doc).item(0).getTextContent();
@@ -103,7 +256,7 @@ public class xmlModifier {
             command.execute();
         } catch (Exception e){
             e.printStackTrace();
-        }
+        }*/
         return 0;
     }
 
@@ -138,7 +291,7 @@ public class xmlModifier {
         }
         return doc;
     }
-    
+
     private static void modifyDocument(Document doc, String path){
         TransformerFactory transformerFactory = TransformerFactory.newInstance();
         try {
@@ -149,6 +302,29 @@ public class xmlModifier {
         } catch (TransformerException e) {
             e.printStackTrace();
         }
+    }
+
+    public static List<String> xmlRetrieverWrapper(String operation, String[] contents){
+        ArrayList<String> tagsToBeRetrieved;
+        switch (operation) {
+            case "LDAP Unit":
+                tagsToBeRetrieved = new ArrayList<>(Arrays.asList(ldapUserForUnitTestXPath, ldapPasswordForUnitTestXPath));
+                break;
+            case "LDAP Gobii":
+                tagsToBeRetrieved = new ArrayList<>(Arrays.asList(
+                        gobiiAuthenticationTypeXPath, ldapUserDnPatternXPath, ldapUrlXPath, ldapBindUserXPath,
+                        ldapBindPasswordXPath, ldapUserForBackendProcsXPath, ldapPasswordForBackendProcsXPath));
+                break;
+            case "Email notification":
+                tagsToBeRetrieved = new ArrayList<>(Arrays.asList(emailSvrTypeXPath, emailSvrDomainXPath, emailSvrUserXPath,
+                        emailSvrHashTypeXPath, emailSvrPasswordXPath, emailServerPortXPath));
+                break;
+            default:
+                //Not implemented yet
+                System.out.println("Queried a not yet implemented modifier.");
+                return null;
+        }
+        return retriever("", tagsToBeRetrieved);
     }
 
     public static void xmlModifierWrapper(String operation, String[] contents){
@@ -214,6 +390,10 @@ public class xmlModifier {
         System.out.println("Test 8");
         String[] testContent_8 = {"user2", "dummypass"};
         xmlModifierWrapper("LDAP Unit",  testContent_8);
+    }
+
+    public String getIncludeSrc() {
+        return includeSrc;
     }
 }
 
