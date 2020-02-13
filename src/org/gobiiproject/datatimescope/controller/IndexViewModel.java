0905c900e6db3fbd06f420e08ec89d9c9e895ceb
Rodolfo N. Duldulao, Jr <rnduldulaojr@gmail.com>
2020-02-13 18:28:40 +0800

    Refactored prefilling login details using cookie values.

--- src/org/gobiiproject/datatimescope/controller/IndexViewModel.java
+++ src/org/gobiiproject/datatimescope/controller/IndexViewModel.java
@@ -9,6 +9,10 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.log4j.Logger;
 import org.gobiiproject.datatimescope.entity.ServerInfo;
 import org.gobiiproject.datatimescope.services.ViewModelServiceImpl;
@@ -29,40 +33,67 @@ public class IndexViewModel {
 
 	@Init
 	public void init() {
-		
+		log.debug("Initializing Index page");
 		viewModelService = new ViewModelServiceImpl();
 		
 		setDatawarehouseVersion(viewModelService.getDatawarehouseVersion());
-		setServerInfo(new ServerInfo());
-		setServerInfo((ServerInfo) Sessions.getCurrent().getAttribute("serverInfo"));
+		ServerInfo current = (ServerInfo) Sessions.getCurrent().getAttribute("serverInfo");
+		if (current == null || current.getHost() == null || current.getHost() == "" ) {
+			log.info("Loading default DB values from cookie");
+			ServerInfo serverInfo = new ServerInfo();
+			//Load cookies --
+			HttpServletRequest request = (HttpServletRequest) Executions.getCurrent().getNativeRequest();
+			Cookie[] cookies = request.getCookies();
 
+			for (Cookie cookie: cookies) {
+				if (cookie.getName().equals("DB_HOST")) {
+					serverInfo.setHost(cookie.getValue());
+				}
+				else if (cookie.getName().equals("DB_PORT")) {
+					serverInfo.setPort(cookie.getValue());
+				}
+				else if (cookie.getName().equals("DB_NAME")) {
+					serverInfo.setDbName(cookie.getValue());
+				}
+			}
 
-		File configFile = new File( System.getProperty("user.dir")+"/config.properties");
-		try {
-			//TODO: Move this to startup servlet and store the contents of config.properties to application scope.
-			ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-			InputStream reader = classLoader.getResourceAsStream("config.properties");
-			
-		    //FileReader reader = new FileReader(configFile);
-		    Properties props = new Properties();
-		    props.load(reader);
-		 
-		    String tsversion = props.getProperty("version");
-
-		    if(tsversion != null){
-		    	setVersion(tsversion);
-		    }
-		    reader.close();
-		} catch (FileNotFoundException ex) {
-		    // file does not exist
-			 log.error("cannot find config properties file: "+ configFile.getAbsolutePath());
-		} catch (IOException ex) {
-		    // I/O error
-			 log.error("i/o exception"+ ex.getMessage());
-		} catch (NullPointerException ex) {
-		    // file does not exist
-			 log.error("Null values were retrieved from config properties file: "+ configFile.getAbsolutePath());
+			setServerInfo(serverInfo);
 		}
+		//setServerInfo((ServerInfo) Sessions.getCurrent().getAttribute("serverInfo"));
+		//getting the version
+		ServletContext context = Executions.getCurrent().getDesktop().getWebApp().getServletContext();
+		String version = (String) context.getAttribute("VERSION");
+		if (version != null) {
+			setVersion(version);
+		}
+
+//
+//		//File configFile = new File( System.getProperty("user.dir")+"/config.properties");
+//		try {
+//			//TODO: Move this to startup servlet and store the contents of config.properties to application scope.
+//			ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+//			InputStream reader = classLoader.getResourceAsStream("config.properties");
+//			
+//		    //FileReader reader = new FileReader(configFile);
+//		    Properties props = new Properties();
+//		    props.load(reader);
+//		 
+//		    String tsversion = props.getProperty("version");
+//
+//		    if(tsversion != null){
+//		    	setVersion(tsversion);
+//		    }
+//		    reader.close();
+//		} catch (FileNotFoundException ex) {
+//		    // file does not exist
+//			 log.error("cannot find config properties file: "+ configFile.getAbsolutePath());
+//		} catch (IOException ex) {
+//		    // I/O error
+//			 log.error("i/o exception"+ ex.getMessage());
+//		} catch (NullPointerException ex) {
+//		    // file does not exist
+//			 log.error("Null values were retrieved from config properties file: "+ configFile.getAbsolutePath());
+//		}
 	}
 
 	
@@ -89,6 +120,7 @@ public class IndexViewModel {
 	}
 
 	public void setServerInfo(ServerInfo serverInfo) {
+		log.debug(String.format("Set serverInfo called: %s %s %s", serverInfo.getHost(), serverInfo.getPort(), serverInfo.getDbName()));
 		this.serverInfo = serverInfo;
 	}
 
