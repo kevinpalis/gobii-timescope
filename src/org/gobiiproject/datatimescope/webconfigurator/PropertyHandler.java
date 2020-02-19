dc693afbfda5856b75b4fd38ca9a52f8bebbe931
Rodolfo N. Duldulao, Jr <rnduldulaojr@gmail.com>
2020-02-19 10:59:19 +0800

    Refactored hardcoded script paths

--- src/org/gobiiproject/datatimescope/webconfigurator/PropertyHandler.java
+++ src/org/gobiiproject/datatimescope/webconfigurator/PropertyHandler.java
@@ -3,6 +3,8 @@ package org.gobiiproject.datatimescope.webconfigurator;
 import java.io.*;
 import java.util.Properties;
 
+import org.zkoss.zk.ui.Executions;
+
 import static org.gobiiproject.datatimescope.webconfigurator.UtilityFunctions.writeToLog;
 
 /**
@@ -11,6 +13,7 @@ import static org.gobiiproject.datatimescope.webconfigurator.UtilityFunctions.wr
 
 public class PropertyHandler{
 
+	
     private Properties prop = new Properties();
     private String username;
 
@@ -22,9 +25,15 @@ public class PropertyHandler{
         return prop.getProperty("username");
     }
 
+    private String getPropertiesFilePath() {
+    	String propsPath = Executions.getCurrent().getDesktop().getWebApp().getRealPath("./WEB-INF/classes/gobii-configurator.properties");
+    	return propsPath;
+    }
+    
     public void setUsername(String username){
         try {
-            OutputStream output = new FileOutputStream("/usr/local/tomcat/webapps/timescope/WEB-INF/classes/gobii-configurator.properties");
+        	OutputStream output = new FileOutputStream(getPropertiesFilePath());
+            //OutputStream output = new FileOutputStream("/usr/local/tomcat/webapps/timescope/WEB-INF/classes/gobii-configurator.properties");
             //OutputStream output = new FileOutputStream("/home/fvgoldman/gobiidatatimescope/out/artifacts/gobiidatatimescope_war_exploded/WEB-INF/classes/gobii-configurator.properties");
             prop.setProperty("username" , username);
             prop.store(output, null);
@@ -44,7 +53,8 @@ public class PropertyHandler{
 
     public void setPassword(String password){
         try {
-            OutputStream output = new FileOutputStream("/usr/local/tomcat/webapps/timescope/WEB-INF/classes/gobii-configurator.properties");
+        	OutputStream output = new FileOutputStream(getPropertiesFilePath());
+            //OutputStream output = new FileOutputStream("/usr/local/tomcat/webapps/timescope/WEB-INF/classes/gobii-configurator.properties");
             //OutputStream output = new FileOutputStream("/home/fvgoldman/gobiidatatimescope/out/artifacts/gobiidatatimescope_war_exploded/WEB-INF/classes/gobii-configurator.properties");
             prop.setProperty("password" , password);
             prop.store(output, null);
